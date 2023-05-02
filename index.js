59105d6b4f958ed9a9849ab24a2fb8785140115c
joshau kregg <kreggjoshua@gmail.com>
2023-05-02 16:20:49 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+      let array = word.split('')
+      
+      array[0] = array[0].toUpperCase()
+      
+      array = array.join('')
+      return array
+  }
+  static sanitize(string) {
+
+    let array = string.split(' ')
+
+    for( let i = 0; i < array.length; i++) {
+    array[i] = array[i].replace(/[^A-Za-z0-9-']+/g, "");
+    }
+    array = array.join(' ')
+   
+    return array
+  }
+  static titleize(string) {
+    let array = string.split(' ')
+    let i = 0
+    array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+    for(  i = 1; i < array.length; i++) {
+      if(array[i] != "the" && array[i] != "a" && array[i] != "but" && array[i] != "of"&& array[i] != "and"&& array[i] != "for"&& array[i] != "at"&& array[i] != "by"&& array[i] != "from"&& array[i] != "an") {
+    array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+      }
+    }
+    array = array.join(' ')
+   console.log(array)
+    return array
+  }
 }
